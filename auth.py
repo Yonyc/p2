5d592ea0a0df9d1edd0ff65a1926ae3b88ef8960
Yonyc <arnaudwery24@gmail.com>
2022-04-27 09:39:51 +0200

    Modified import db_init

--- auth.py
+++ auth.py
@@ -5,7 +5,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db,init_db
+from db_init import get_db,init_db
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
