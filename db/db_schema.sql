e5e6f4cd99519d4938f214e333c951be2746267d
JarodRoss <jarodross852@gmail.com>
2022-04-27 09:11:11 +0200

    Edit db schema

--- db/db_schema.sql
+++ db/db_schema.sql
@@ -1,148 +1,75 @@
--- -----------------------------------------------------
--- Schema farm
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `farm` ;
-USE `farm` ;
-
--- -----------------------------------------------------
--- Table `farm`.`familles`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `farm`.`familles` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS animaux;
+DROP TABLE IF EXISTS familles;
+DROP TABLE IF EXISTS types;
+DROP TABLE IF EXISTS animaux_types;
+DROP TABLE IF EXISTS velages;
+DROP TABLE IF EXISTS animaux_velages;
+DROP TABLE IF EXISTS complications;
+DROP TABLE IF EXISTS velages_complications;
 
 
--- -----------------------------------------------------
--- Table `farm`.`animaux`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `farm`.`animaux` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `famille_id` INT NOT NULL,
-  `sexe` VARCHAR(1) NULL,
-  `presence` TINYINT NULL,
-  `apprivoise` TINYINT NULL,
-  `mort_ne` TINYINT NULL,
-  `decede` TINYINT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_animaux_familles_idx` (`famille_id` ASC) VISIBLE,
-  CONSTRAINT `fk_animaux_familles`
-    FOREIGN KEY (`famille_id`)
-    REFERENCES `farm`.`familles` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
 
+CREATE TABLE animaux (
+  id            INTEGER             NOT NULL,
+  famille_id    INTEGER             NOT NULL,
+  sexe          TEXT                NOT NULL,
+  presence      TEXT                NOT NULL,
+  apprivoise    TEXT                NOT NULL,
+  mort_ne       TEXT                NOT NULL,
+  decede        TEXT                NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (famille_id) REFERENCES familles(id)
+);
 
--- -----------------------------------------------------
--- Table `farm`.`types`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `farm`.`types` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `type` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+CREATE TABLE familles (
+  id            INTEGER             NOT NULL,
+  nom           TEXT                NOT NULL,
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE types (
+  id            INTEGER             NOT NULL,
+  type          TEXT                NOT NULL,
+  PRIMARY KEY (id)
+);
 
--- -----------------------------------------------------
--- Table `farm`.`animaux_types`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `farm`.`animaux_types` (
-  `animal_id` INT NOT NULL,
-  `type_id` INT NOT NULL,
-  `pourcentage` INT NULL,
-  INDEX `fk_animaux_types_animaux1_idx` (`animal_id` ASC) VISIBLE,
-  INDEX `fk_animaux_types_types1_idx` (`type_id` ASC) VISIBLE,
-  CONSTRAINT `fk_animaux_types_animaux1`
-    FOREIGN KEY (`animal_id`)
-    REFERENCES `farm`.`animaux` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_animaux_types_types1`
-    FOREIGN KEY (`type_id`)
-    REFERENCES `farm`.`types` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE animaux_types (
+  animal_id     INTEGER             NOT NULL,
+  type_id       INTEGER             NOT NULL,
+  pourcentage   REAL                NOT NULL,
+  PRIMARY KEY (animal_id,type_id),
+  FOREIGN KEY (animal_id) REFERENCES animaux(id),
+  FOREIGN KEY (type_id) REFERENCES types(id)
+);
 
+CREATE TABLE velages (
+  id            INTEGER             NOT NULL,
+  mere_id       INTEGER             NOT NULL,
+  pere_id       INTEGER             NOT NULL,
+  date          TEXT                NOT NULL,
+  FOREIGN KEY (mere_id) REFERENCES animaux(id),
+  FOREIGN KEY (pere_id) REFERENCES animaux(id),
+  PRIMARY KEY (id)
+);
 
--- -----------------------------------------------------
--- Table `farm`.`velages`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `farm`.`velages` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `mere_id` INT NOT NULL,
-  `pere_id` INT NOT NULL,
-  `date_naissance` DATE NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_velages_animaux1_idx` (`mere_id` ASC) VISIBLE,
-  INDEX `fk_velages_animaux2_idx` (`pere_id` ASC) VISIBLE,
-  CONSTRAINT `fk_velages_animaux1`
-    FOREIGN KEY (`mere_id`)
-    REFERENCES `farm`.`animaux` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_velages_animaux2`
-    FOREIGN KEY (`pere_id`)
-    REFERENCES `farm`.`animaux` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE animaux_velages (
+  animal_id     INTEGER             NOT NULL,
+  velage_id     INTEGER             NOT NULL,
+  PRIMARY KEY (animal_id,velage_id),
+  FOREIGN KEY (animal_id) REFERENCES animaux(id),
+  FOREIGN KEY (velage_id) REFERENCES velages(id)
+);
 
+CREATE TABLE complications (
+  id            INTEGER             NOT NULL,
+  complication  TEXT                NOT NULL,
+  PRIMARY KEY (id)
+);
 
--- -----------------------------------------------------
--- Table `farm`.`animaux_velages`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `farm`.`animaux_velages` (
-  `annimal_id` INT NOT NULL,
-  `velage_id` INT NOT NULL,
-  INDEX `fk_animaux_velages_animaux1_idx` (`annimal_id` ASC) VISIBLE,
-  INDEX `fk_animaux_velages_velages1_idx` (`velage_id` ASC) VISIBLE,
-  CONSTRAINT `fk_animaux_velages_animaux1`
-    FOREIGN KEY (`annimal_id`)
-    REFERENCES `farm`.`animaux` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_animaux_velages_velages1`
-    FOREIGN KEY (`velage_id`)
-    REFERENCES `farm`.`velages` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `farm`.`complications`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `farm`.`complications` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `complication` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `farm`.`velages_complications`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `farm`.`velages_complications` (
-  `velage_id` INT NOT NULL,
-  `complication_id` INT NOT NULL,
-  INDEX `fk_velages_complications_velages1_idx` (`velage_id` ASC) VISIBLE,
-  INDEX `fk_velages_complications_complications1_idx` (`complication_id` ASC) VISIBLE,
-  CONSTRAINT `fk_velages_complications_velages1`
-    FOREIGN KEY (`velage_id`)
-    REFERENCES `farm`.`velages` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_velages_complications_complications1`
-    FOREIGN KEY (`complication_id`)
-    REFERENCES `farm`.`complications` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE velages_complications (
+  velage_id        INTEGER             NOT NULL,
+  complication_id  INTEGER             NOT NULL,
+  PRIMARY KEY (complication_id,velage_id),
+  FOREIGN KEY (complication_id) REFERENCES complications(id),
+  FOREIGN KEY (velage_id) REFERENCES velages(id)
+);
