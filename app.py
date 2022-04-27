be5f5beb9f4b8db09ce9bbabd3a167ea1041ab7a
JarodRoss <jarodross852@gmail.com>
2022-04-27 09:38:20 +0200

    Add app config

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #Modules
 import flask
+import os
 from . import db
 from . import auth
 
@@ -8,6 +9,10 @@ def create_app():
     Fonction permettant de créer l'app flask.
     """
     app = flask.Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
     db.init_app(app)
 
