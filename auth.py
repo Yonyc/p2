b9074a08617873aef96276307dcb1d7273e163f4
Yonyc <arnaudwery24@gmail.com>
2022-04-27 09:05:37 +0200

    Created /velage route

--- auth.py
+++ auth.py
@@ -42,12 +42,32 @@ def complication():
         nb_complication.append(len(nb_db))
     return render_template('auth/complication.html',data = data,nb_complication = nb_complication,list_complication = list_complication)
 
+@bp.route('/velage', methods=('GET', 'POST'))
+def velage():
 
-
-
-
-
-
-
-
-
+    if request.method == 'POST':
+        print('clicked')
+        print(request.form.get("Famille"))
+        print(request.form.get("Sexe"))
+        print(request.form.get("Année"))
+        print(request.form.get("Mois"))
+        Famille = request.form.get("Famille")
+        Sexe = request.form.get("Sexe")
+        Année = request.form.get("Année")
+        Mois  = request.form.get("Mois")
+        return redirect(url_for("auth.affich_graph",Famille = Famille,Sexe = Sexe,Mois = Mois, Ans = Année))
+    else:
+        année = []
+        fam = []
+        db = get_db()
+        cur =  db.execute('SELECT * FROM velages')
+        for velage in cur.fetchall():
+            ans = int(velage[3][6:])
+            if ans not in année:
+                année.append(ans)
+        année.sort()
+        cur =  db.execute('SELECT * FROM familles')
+        for famille in cur.fetchall():
+            fam.append(famille[1])
+        fam.sort()
+        return render_template('auth/velage.html', année = année, fam = fam)
