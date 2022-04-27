39fb2a9e5457b5c25bb19f2ac24a6b486d676e44
Yonyc <arnaudwery24@gmail.com>
2022-04-27 09:06:34 +0200

    Created route for animal info

--- auth.py
+++ auth.py
@@ -71,3 +71,49 @@ def velage():
             fam.append(famille[1])
         fam.sort()
         return render_template('auth/velage.html', année = année, fam = fam)
+
+@bp.route("/<Famille>/<Sexe>/<Mois>/<Ans>", methods=('GET', 'POST'))
+def affich_graph(Famille, Sexe,Mois,Ans):
+    if request.method == 'POST':
+        print('clicked')
+        print(request.form.get("Famille"))
+        print(request.form.get("Sexe"))
+        print(request.form.get("Année"))
+        print(request.form.get("Mois"))
+        Famille = request.form.get("Famille")
+        Sexe = request.form.get("Sexe")
+        Année = request.form.get("Année")
+        Mois  = request.form.get("Mois")
+        return redirect(url_for("auth.affich_graph",Famille = Famille, Sexe = Sexe,Mois = Mois,Ans = Année))
+    else:
+        année = []
+        fam = []
+        db = get_db()
+        cur =  db.execute('SELECT * FROM velages')
+        for velage in cur.fetchall():
+            ans = int(velage[3][6:])
+            if ans not in année:
+                année.append(ans)
+        année.sort()
+        cur =  db.execute('SELECT * FROM familles')
+        for famille in cur.fetchall():
+            fam.append(famille[1])
+        fam.sort()
+
+        #suite 
+        vel = []
+        list_complication = ['"Aide vétérinaire au vêlage"', '"Vêlage difficile"', '"Mère avec post complication"', '"Veau trop petit à la naissance"', '"Veau mal placé"', '"Vêlage plus tôt que prévu"', '"Veau trop gros à la naissance"', '"Veau avec post complication"', '"Veau mal formé"']
+        if Famille == "Toutes les familles" and Sexe == "Male et femelle" and Ans == "Toutes les années" and Mois == "Tous les mois":
+            cur =  db.execute("SELECT velages.id, velages.date, velages_complications.complication_id FROM velages INNER JOIN velages_complications ON velages.id = velages_complications.velage_id")
+            for velage in cur.fetchall():
+                vel.append(velage[2])
+            nb_complication = [vel.count(1),vel.count(2),vel.count(3),vel.count(4),vel.count(5),vel.count(6),vel.count(7),vel.count(8),vel.count(9)]
+            print(nb_complication)
+            
+        elif Famille != "Toutes les familles" and Sexe == "Male et femelle" and Ans == "Toutes les années" and Mois == "Tous les mois":
+            cur =  db.execute("SELECT velages.id, velages.date, velages_complications.complication_id FROM velages INNER JOIN velages_complications ON velages.id = velages_complications.velage_id ")
+            for velage in cur.fetchall():
+                vel.append(velage[2])
+            nb_complication = [vel.count(1),vel.count(2),vel.count(3),vel.count(4),vel.count(5),vel.count(6),vel.count(7),vel.count(8),vel.count(9)]
+            print(nb_complication)
+    return render_template('auth/afiche.html',Famille = Famille , Sexe = Sexe, année = année, fam = fam,Mois = Mois,Ans = Ans)
